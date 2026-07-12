138b74f7cca56fe74cf1566d492aa479fb446ce2
Glory17805 <glorysrujitha@gmail.com>
2026-07-12 23:43:24 +0530

    Added Swagger JWT authorization configuration

---
+++ backend/src/main/java/com/bookstore/config/OpenApiConfig.java
@@ -0,0 +1,37 @@
+package com.bookstore.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI bookstoreOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Book Store Management API")
+                        .version("1.0")
+                        .description("REST API documentation for Book Store Management System"))
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList(SECURITY_SCHEME_NAME))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        SECURITY_SCHEME_NAME,
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
+    }
+}
